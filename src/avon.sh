8e76b4d2ca8e8aaa482d0b95ae6ec47c43d5e2e3
Tavin Turner <camelinatardis@gmail.com>
2019-04-12 10:30:32 -0600

    Automatic updates

--- src/avon.sh
+++ src/avon.sh
@@ -1,1 +1,24 @@
 #!/usr/bin/env bash
+
+mkdir avon avon/log avon/dump
+logf=./avon/log/avon_$(date +%T).log
+dump=./avon/dump
+
+# Logging
+log() {
+  echo $(date +%T): $1 >> $logf
+  echo $1
+}
+
+# Ensure script is running as root
+if [ $EUID -ne 0 ]; then
+  log "Run as root"
+  exit 64
+fi
+
+# Automatic updates
+autoupdates() {
+  log "Enabling automatic updates"
+  cat presets/auto-upgrades > /etc/apt/apt.conf.d/20auto-upgrades
+  cat presets/sources.list > /etc/apt/sources.list
+}
