0e49f6b4351d224adaa25c988017292275029a4f
Cutwow475 <31255827+Cutwow@users.noreply.github.com>
2019-11-21 08:29:08 -0700

    Update bailey.sh

--- src/bailey.sh
+++ src/bailey.sh
@@ -415,7 +415,7 @@ configuring_file_permissions_function() {
 }
 
 etc_passwd_config() {configuring_file_permissions_function /etc/passwd 644 root}
-etc_shadow_config_16() {configuring_file_permissions_function /etc/shadow o-rwx,g-rwx shadow}
+etc_shadow_config() {configuring_file_permissions_function /etc/shadow o-rwx,g-rwx shadow}
 etc_group_config() {configuring_file_permissions_function /etc/group 644 root}
 etc_gshadow_config() {configuring_file_permissions_function /etc/gshadow o-rwx,g-rwx shadow}
 etc_passwd_-_config() {configuring_file_permissions_function /etc/passwd- 600 root}
